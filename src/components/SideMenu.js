caa6e7fdd319c5fc91c26ec432aa60b8cb797119
Varen Aggarwal <varenaggarwal@gmail.com>
2021-04-04 11:35:16 +0530

    add side menu Links

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,32 +1,41 @@
+import { Link } from "react-router-dom";
+
 export function SideMenu() {
   return (
     <div className="side-menu">
-      {/* <h1>Side Menu</h1> */}
       <ul className="list list-menu">
-        <li>
-          <span>
-            <i className="fa fa-fas fa-home"></i>
-          </span>
-          <span>Home</span>
-        </li>
-        <li>
-          <span>
-            <i className="fas fa-history"></i>
-          </span>
-          <span> Watch History</span>
-        </li>
-        <li>
-          <span>
-            <i className="fas fa-bookmark"></i>
-          </span>
-          <span>Bookmarked</span>
-        </li>
-        <li>
-          <span>
-            <i className="fas fa-list"></i>
-          </span>
-          <span>Playlists</span>
-        </li>
+        <Link to="/">
+          <li>
+            <span>
+              <i className="fa fa-fas fa-home"></i>
+            </span>
+            <span>Home</span>
+          </li>
+        </Link>
+        <Link to="/history">
+          <li>
+            <span>
+              <i className="fas fa-history"></i>
+            </span>
+            <span> Watch History</span>
+          </li>
+        </Link>
+        <Link to="/bookmarked">
+          <li>
+            <span>
+              <i className="fas fa-bookmark"></i>
+            </span>
+            <span>Bookmarked</span>
+          </li>
+        </Link>
+        <Link to="/playlists">
+          <li>
+            <span>
+              <i className="fas fa-list"></i>
+            </span>
+            <span>Playlists</span>
+          </li>
+        </Link>
       </ul>
     </div>
   );
